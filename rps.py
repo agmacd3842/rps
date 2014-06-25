0af61bd7699b7ae6afd3a118365479ae2cd89189
unknown <agmacd3842@SCI-257LB-6Y2H.ad.uauth.net>
2014-06-25 19:15:34 -0400

    Added player names and end phrase.

--- rps.py
+++ rps.py
@@ -20,4 +20,15 @@ def compare(choice1, choice2):
     elif choice1 == 'paper':
         if choice2 == 'scissors':
             print("scissors wins")
-    
+
+rounds = int(raw_input("enter number of rounds"))
+player1_name = raw_input("enter player1 name: ")
+player2_name = raw_input("enter player2 name: ")
+counter = 0
+while counter != rounds:
+    choice1 = choice()
+    choice2 = choice()
+    choice3 = choice()
+    compare(choice1, choice2)
+    counter +=1
+print ("{} and {} would you like to play again?".format(player1_name, player2_name))
